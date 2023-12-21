a1988a43f40fed62840820f2241dcb17fd73e245
Danilo <dudu.foresti@gmail.com>
2023-12-21 09:17:34 +0100

    Inizio gioco versione client

--- Perudo_Game/src/giocoClient/Game.java
+++ Perudo_Game/src/giocoClient/Game.java
@@ -8,12 +8,24 @@ public class Game {
     static LinkedList<Integer> results = new LinkedList<>();
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int nDice;
+        int nSearch;
+        System.out.println("Inserisci il numero da cercare");
+        nSearch = Integer.parseInt(s.nextLine());
+        System.out.println("Inserisci il numero di dadi");
+        nDice = Integer.parseInt(s.nextLine());
         playerList.add(new Player("eda27", 5));
         playerList.add(new Player("Samuele", 5));
         playerList.add(new Player("Jacopo", 5));
         playerList.add(new Player("Andrea", 5));
         rollAll();
         printAll();
+        if(count(nSearch)<nDice){
+            System.out.println("Hai perso");
+        }
+        else {
+            System.out.println("Hai vinto");
+        }
     }
 
     private static void rollAll(){
